1a5b964fc9866facf703d51ac207f72f8acb40fe
Student <bastiendallago@hotmail.fr>
2025-03-25 09:37:48 +0100

    test change main in branch bastien-main

--- main.c
+++ main.c
@@ -6,11 +6,21 @@
 /*   By: student <student@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 09:20:21 by student           #+#    #+#             */
-/*   Updated: 2025/03/25 09:21:00 by student          ###   ########.fr       */
+/*   Updated: 2025/03/25 09:36:33 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int main(int argc, char **argv)
 {
+    int    i;
+    
+    i = argc;
+    (void)argv;
+    while (i < 10)
+    {
+        printf("i = %d\n", i++);
+    }
     return (0);
 }
